16f728586e05bc265567bda94ad5c5651836ea53
Aaron Winter <winter.aaron93@gmail.com>
2020-04-04 11:30:47 +0200

    added option to not transition between scenes

--- addons/scene_manager/source/Scene.gd
+++ addons/scene_manager/source/Scene.gd
@@ -3,8 +3,9 @@ extends CanvasLayer
 var scenes := {}
 
 var next_scene := ""
-var show_bar := false
 
+var show_bar := false
+var show_transition := true
 var changing := false
 
 onready var scene_loader := $SceneLoader as SceneLoader
@@ -14,7 +15,7 @@ onready var progress_bar := $TextureProgress as TextureProgress
 func register_scene(scene_name: String, path_to_scene: String) -> void:
 	scenes[scene_name] = path_to_scene
 
-func change(scene_name: String, show_bar: bool = false) -> void:
+func change(scene_name: String, show_transition := true, show_bar: bool = false) -> void:
 
 	if changing:
 		return
@@ -24,11 +25,17 @@ func change(scene_name: String, show_bar: bool = false) -> void:
 		return
 
 	self.show_bar = show_bar
+	self.show_transition = show_transition
+
 	next_scene = scenes[scene_name]
 
 	print("loading scene: %s" % next_scene)
 
-	anim.play("fade_out")
+	if show_transition:
+		anim.play("fade_out")
+	else:
+		_on_AnimationPlayer_animation_finished("fade_out")
+
 	changing = true
 
 func _on_AnimationPlayer_animation_finished(anim_name: String) -> void:
@@ -44,7 +51,8 @@ func _on_ProgressBar_value_changed(value: float) -> void:
 
 func _on_SceneLoader_scene_loaded(scene) -> void:
 	get_tree().change_scene_to(scene)
-	anim.play("fade_in")
+	if show_transition:
+		anim.play("fade_in")
 	progress_bar.visible = false
 	next_scene = ""
 	changing = false
--- source/menu/TitleScreen.gd
+++ source/menu/TitleScreen.gd
@@ -2,7 +2,7 @@ extends Panel
 
 
 func _on_Play_pressed() -> void:
-	Scene.change("Game")
+	Scene.change("Game", false)
 
 func _on_Quit_pressed() -> void:
 	get_tree().quit()
